4aeafdeb40613f02c16b7089c9eeb287be8889ab
John Gallagher <john@foursquare.com>
2012-08-09 14:40:20 -0400

    add SIGQUIT handlers to the carbons

--- bin/carbon-relay.py
+++ bin/carbon-relay.py
@@ -27,4 +27,21 @@ sys.path.insert(0, LIB_DIR)
 
 from carbon.util import run_twistd_plugin
 
+import threading
+import traceback
+def dumpstacks(signal, frame):
+    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""), threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    print "\n".join(code)
+
+import signal
+signal.signal(signal.SIGQUIT, dumpstacks)
+
+
 run_twistd_plugin(__file__)
